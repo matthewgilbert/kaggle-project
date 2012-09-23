a5ebf517a1a92931f602f6be7d4ae5b5435c7e74
Cameron Davidson-Pilon <cam.davidson.pilon@gmail.com>
2012-09-22 23:46:50 -0400

    edits to .py

--- data_exploring.py
+++ data_exploring.py
@@ -2,40 +2,66 @@
 Kaggle Census competition
 """
 
+import math
 
+import pandas 
+import sklearn.linear_model as sklm
 
-import pandas #good data tables, like R dataframes
 import census_utilities 
 
 
-
+#open training_file
 data = pandas.read_csv( "training_filev1.csv")
 response = data['Mail_Return_Rate_CEN_2010']
 weights = data['weight']
 
 
 
-#Some of the data is not really useful in a regression (state_id, state_name etc), 
-# so lets delete it. Also remove the response variable.
-to_remove = ['Mail_Return_Rate_CEN_2010', 'State', 'State_name', 'County_name', 
-             'County', 'GIDBG', 'Tract', 'Block_Group', 'Flag', 'weight']
-for to_rm in to_remove:
-    del data[to_rm]
 
-    
-to_convert_to_float = ['Med_HHD_Inc_BG_ACS_06_10', 'Med_HHD_Inc_BG_ACSMOE_06_10', 'Med_HHD_Inc_TR_ACS_06_10', 'Med_HHD_Inc_TR_ACSMOE_06_10',
- 'Aggregate_HH_INC_ACS_06_10', 'Aggregate_HH_INC_ACSMOE_06_10', 'Med_House_Val_BG_ACS_06_10','Med_House_Val_BG_ACSMOE_06_10''Med_house_val_tr_ACS_06_10',
-'Med_house_val_tr_ACSMOE_06_10', 'Aggr_House_Value_ACS_06_10', 'Aggr_House_Value_ACSMOE_06_10']
 
-for to_cn in to_convert_to_float:
-    data[to_cn] = data[to_cn].map( census_utilities.money2float )
-    
-
-#do a naive filling of missing data
-data.fillna( method="bfill", inplace=True)
-data.fillna( method="ffill", inplace=True)
-
+#preprocess data.
+data = census_utilities.preprocess_dataframe( data )
 
 #Ok data is pretty clean, what can we do with it?
+
+n, d = data.shape
+
+#split the data into two. Replace this with cross-validation eventually.
+training_data = data[ :math.floor( 0.8*n) ]
+training_response = response[ :math.floor( 0.8*n) ]
+
+test_data = data[ math.floor( 0.8*n):]
+test_response = response[ math.floor( 0.8*n): ]
+
+training_weights = weights[:math.floor( 0.8*n) ]
+testing_weights = weights[ math.floor( 0.8*n): ]
+
+
+
+#fit a linear model
+print "Create a linear model."
+lr = sklm.LinearRegression( normalize = True )
+lr.fit( training_data, training_response )
+
+prediction = lr.predict( test_data )
+
+print "Cross validation: %f" %census_utilities.WMAEscore( prediction, test_response, testing_weights )
+
+
+#open the testing file
+test_data = pandas.read_csv( "testing_filev1.csv")
+test_data = census_utilities.preprocess_dataframe( test_data )
+
+
+lr.fit( data, response )
+lr.predict( test_data)
+
+
+
+
+
+
+
+
 
 
