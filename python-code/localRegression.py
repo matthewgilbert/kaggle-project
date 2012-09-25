4a08d48706a8fac4eb3a60a90b9ca69a309776b4
Cameron Davidson-Pilon <cam.davidson.pilon@gmail.com>
2012-09-25 19:34:31 -0400

    localRegression.py added

---
+++ python-code/localRegression.py
@@ -0,0 +1,63 @@
+
+
+import pandas
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+import census_utilities
+import geoNN
+
+
+class LocalRegression( object ):
+    """implements a scikitlearn model that finds nearest geographic neighbours and computes a regression. Defaults
+    to a global regression if location data is not available.
+   
+    
+    methods:
+        fit( data, response, location_data )
+        predict( data, location_data)
+    
+    """
+    
+    def __init__(self, k = 200, regressor = LinearRegression ):
+        self.k = k
+        self.regressor = regressor
+        self.global_regressor_ = self.regressor()
+        
+    def fit(self, data, response, location_data):
+        self.data_ = data
+        self.response_ = response
+        self.location_data_ = location_data
+        
+        self.global_regressor_.fit( data, response)
+        
+        self.gnn = geoNN.GeoFinder( location_data)
+        
+        return self
+        
+        
+        
+    def predict( self, data, location_data):
+        
+        if location_data.shape[0] != data.shape[0]:
+            raise Exception("length of first argument does not equal length of second argument.")
+        
+        n = location_data.shape[0]
+        
+        predition = np.empty( n)
+        for i in range(n):
+            location = location_data.values[i,:]
+            if np.isnan( location ).any():
+                #use global regression
+                prediction[i] = self.global_regressor.predict( data.values[i,:] )
+                
+            else:
+                #gather k near data.
+                inx = self.gnn.find( location[0], location[1], self.k )
+                sub_data = data.values[inx,:]
+                sub_response = response.values[inx,:]
+                reg = self.regressor()
+                reg.fit( sub_data, sub_response )
+                prediction[i] = reg.predict( data.values[i,:] )
+                
+                
