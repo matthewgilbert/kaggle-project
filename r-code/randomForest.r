44d6dfe1579dea7905a5438632e6e805fe9d6ed9
MatthewGilbert <matthew.gilbert12@gmail.com>
2012-10-03 16:11:28 -0400

    minor

--- r-code/randomForest.r
+++ r-code/randomForest.r
@@ -1,13 +1,13 @@
 ####Fit RF Model############
 library(randomForest)
 load("cleaned.dat")
-index_train = grep("GIDBG|weight", names(census.formatted.df), invert=TRUE)
+index_train = grep("GIDBG|LATITUDE|LONGITUDE|weight", names(census.formatted.df), invert=TRUE)
 data = census.formatted.df[,index_train]
 fit.rf <- randomForest(Mail_Return_Rate_CEN_2010 ~ ., data = data )
 
 ####Make Predictions#########
 load("cleanedtest.dat")
-index_test = grep("GIDBG|weight", names(test.census.formatted.df), invert=TRUE)
+index_test = grep("GIDBG|LATITUDE|LONGITUDE|weight", names(test.census.formatted.df), invert=TRUE)
 test.data = test.census.formatted.df[,index_test]
 predictions = predict(fit.rf, test.data)
 
