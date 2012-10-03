176da8c9e92300ad996629bb6b7324c75b95df4c
MatthewGilbert <matthew.gilbert12@gmail.com>
2012-10-02 21:50:50 -0400

    Some random forest additions, changed clean to a function from a script

--- r-code/clean.r
+++ r-code/clean.r
@@ -1,88 +1,92 @@
-###set data input to 'training' or 'test' set
-series = 'test'
+clean <- function(series) {
+
+    if (series != 'training' && series != 'test')
+        stop('Please input either training or test as the series')
+
+    #Data Loading and Cleaning
+    census.df <- read.csv(file = paste(series, "_filev1.csv", sep=''))
+
+    temp.df = census.df
+
+    index = seq(1,length(names(census.df)))
+
+    #lists of indices to omit, groupped according to different metrics
+    geography_omit = c(1,2,3,4,5,6,7,8)
+    error_omit = grep("MOE",names(census.df))
+    na_indices = c(168,169)
+    na_zero_index = 170
+
+    #change all money indices to numeric
+    money_index = c(124,125,126,127,128,129,161,162,163,164,165,166)
+    for(i in money_index) {
+        temp.df[,i] = as.numeric(temp.df[,i])
+    }
+    #change places which received no English/Spanish ballots from NA to 0
+    temp.df[is.na(temp.df[,na_zero_index]),na_zero_index] = 0
+
+    index = setdiff(setdiff(setdiff(index,geography_omit),error_omit),na_indices)
+    census.cleaned.df = temp.df[,index]
 
 
-#Data Loading and Cleaning
-census.df <- read.csv(file = paste(series, "_filev1.csv", sep=''))
+    #Data Transformation --> changing raw numbers to percentages
 
-temp.df = census.df
+    census.formatted.df = census.cleaned.df
+    unformatted_index = 1:length(census.formatted.df)
+    #non formattable consists of land area stats and median house value stats, return rate and weights
+    non_formattable = c(1,2,75,76,96,97,101,102)
+    pop_index = c(3,4,5)
+    totalPop_index = 6
+    acs_totalPop_index = 7
+    acs_totalHouse_index = 99
 
-index = seq(1,length(names(census.df)))
+    unformatted_index = setdiff(setdiff(setdiff(setdiff(unformatted_index,non_formattable),totalPop_index),acs_totalPop_index),acs_totalHouse_index)
 
-#lists of indices to omit, groupped according to different metrics
-geography_omit = c(1,2,3,4,5,6,7,8)
-error_omit = grep("MOE",names(census.df))
-na_indices = c(168,169)
-na_zero_index = 170
+    census.formatted.df[,pop_index] = census.formatted.df[,pop_index]/census.formatted.df[,totalPop_index]
+    unformatted_index = setdiff(unformatted_index,pop_index)
 
-#change all money indices to numeric
-money_index = c(124,125,126,127,128,129,161,162,163,164,165,166)
-for(i in money_index) {
-    temp.df[,i] = as.numeric(temp.df[,i])
+    #get all population acs stats
+    acs_index = intersect(grep("ACS",names(census.formatted.df)), unformatted_index)
+    acs_pop_index = acs_index[(acs_index <= 49)]
+    acs_house_index = setdiff(acs_index, acs_pop_index)
+
+    no_pop_index = which(census.formatted.df[,acs_totalPop_index] == 0)
+    keep = setdiff(1:nrow(census.formatted.df), no_pop_index)
+    census.formatted.df = census.formatted.df[keep,]
+
+    census.formatted.df[,acs_pop_index] = census.formatted.df[,acs_pop_index] / census.formatted.df[,acs_totalPop_index]
+    unformatted_index = setdiff(unformatted_index,acs_pop_index)
+
+    census.formatted.df[,acs_house_index] = census.formatted.df[,acs_house_index] / census.formatted.df[,acs_totalHouse_index]
+    unformatted_index = setdiff(unformatted_index,acs_house_index)
+
+    spanish_ballot_index = 100
+    census.formatted.df[,spanish_ballot_index] = census.formatted.df[,spanish_ballot_index] / census.formatted.df[,acs_totalHouse_index]
+    unformatted_index = setdiff(unformatted_index,spanish_ballot_index)
+
+    #I believe that acs vs. non acs total housing is the same since the acs refers to sampling period
+    non_acs_housing = unformatted_index[(unformatted_index > 39)]
+    census.formatted.df[,non_acs_housing] = census.formatted.df[,non_acs_housing] / census.formatted.df[,acs_totalHouse_index]
+    unformatted_index = setdiff(unformatted_index,non_acs_housing)
+
+    nonACS_pop_index = unformatted_index
+    census.formatted.df[,nonACS_pop_index] = census.formatted.df[,nonACS_pop_index] / census.formatted.df[,totalPop_index]
+
+    #hack to add in geo coordinates
+    locations <- read.csv(file = paste(series, "_locations.csv", sep=''))
+    census.formatted.df = cbind('LATITUDE'=locations[keep,1], 'LONGITUDE'=locations[keep,2], census.formatted.df)
+    #hack to add back in GidBG so don't have to change all the hardcoded indices above
+    census.formatted.df = cbind('GIDBG'=census.df$GIDBG[keep], census.formatted.df)
+
+    if (series == 'training') {
+        save(census.df, census.cleaned.df, census.formatted.df, file = 'cleaned.dat')
+        write.table(census.formatted.df,"formattedData")
+    } else if (series == 'test') {
+        test.census.df = census.df
+        test.census.cleaned.df = census.cleaned.df
+        test.census.formatted.df = census.formatted.df
+        save(test.census.df, test.census.cleaned.df, test.census.formatted.df, file = 'cleanedtest.dat')
+        write.table(test.census.formatted.df,"formattedDatatest")
+    }
+
 }
-#change places which received no English/Spanish ballots from NA to 0
-temp.df[is.na(temp.df[,na_zero_index]),na_zero_index] = 0
-
-index = setdiff(setdiff(setdiff(index,geography_omit),error_omit),na_indices)
-census.cleaned.df = temp.df[,index]
-
-
-#Data Transformation --> changing raw numbers to percentages
-
-census.formatted.df = census.cleaned.df
-unformatted_index = 1:length(census.formatted.df)
-#non formattable consists of land area stats and median house value stats, return rate and weights
-non_formattable = c(1,2,75,76,96,97,101,102)
-pop_index = c(3,4,5)
-totalPop_index = 6
-acs_totalPop_index = 7
-acs_totalHouse_index = 99
-
-unformatted_index = setdiff(setdiff(setdiff(setdiff(unformatted_index,non_formattable),totalPop_index),acs_totalPop_index),acs_totalHouse_index)
-
-census.formatted.df[,pop_index] = census.formatted.df[,pop_index]/census.formatted.df[,totalPop_index]
-unformatted_index = setdiff(unformatted_index,pop_index)
-
-#get all population acs stats
-acs_index = intersect(grep("ACS",names(census.formatted.df)), unformatted_index)
-acs_pop_index = acs_index[(acs_index <= 49)]
-acs_house_index = setdiff(acs_index, acs_pop_index)
-
-no_pop_index = which(census.formatted.df[,acs_totalPop_index] == 0)
-keep = setdiff(1:nrow(census.formatted.df), no_pop_index)
-census.formatted.df = census.formatted.df[keep,]
-
-census.formatted.df[,acs_pop_index] = census.formatted.df[,acs_pop_index] / census.formatted.df[,acs_totalPop_index]
-unformatted_index = setdiff(unformatted_index,acs_pop_index)
-
-census.formatted.df[,acs_house_index] = census.formatted.df[,acs_house_index] / census.formatted.df[,acs_totalHouse_index]
-unformatted_index = setdiff(unformatted_index,acs_house_index)
-
-spanish_ballot_index = 100
-census.formatted.df[,spanish_ballot_index] = census.formatted.df[,spanish_ballot_index] / census.formatted.df[,acs_totalHouse_index]
-unformatted_index = setdiff(unformatted_index,spanish_ballot_index)
-
-#I believe that acs vs. non acs total housing is the same since the acs refers to sampling period
-non_acs_housing = unformatted_index[(unformatted_index > 39)]
-census.formatted.df[,non_acs_housing] = census.formatted.df[,non_acs_housing] / census.formatted.df[,acs_totalHouse_index]
-unformatted_index = setdiff(unformatted_index,non_acs_housing)
-
-nonACS_pop_index = unformatted_index
-census.formatted.df[,nonACS_pop_index] = census.formatted.df[,nonACS_pop_index] / census.formatted.df[,totalPop_index]
-
-#hack to add back in GidBG so don't have to change all the hardcoded indices above
-census.formatted.df = cbind('GIDBG'=census.df$GIDBG[keep], census.formatted.df)
-
-if (series == 'training') {
-    save(census.df, census.cleaned.df, census.formatted.df, file = 'cleaned.dat')
-    write.table(census.formatted.df,"formattedData")
-} else if (series == 'test') {
-    test.census.df = census.df
-    test.census.cleaned.df = census.cleaned.df
-    test.census.formatted.df = census.formatted.df
-    save(test.census.df, test.census.cleaned.df, test.census.formatted.df, file = 'cleanedtest.dat')
-    write.table(test.census.formatted.df,"formattedDatatest")
-}
-
-
 
--- r-code/testing.r
+++ r-code/testing.r
@@ -1,52 +1,10 @@
-###################################
-# Data loading
-###################################
-
-#setwd("C:\\Documents and Settings\\Administrator\\My Documents\\Dropbox\\census")
-setwd("/home/matthew/Git/kaggle-project/data")
-
-census.df <- read.csv(file = "training_filev1.csv")
-
-
-###################################
-# Data Preprocessing
-###################################
-
-#85 corresponds to non english euro area, 15 is total ACS survey population
-percent_nonenglish = census.df[,91]/census.df[,15]
-
-#83 corresponds to spanish area, 15 is total ACS survey population
-percent_spanish = census.df[,83]/census.df[,15]
-
-percent_mobile = census.df[,151]/census.df[,130]
-
-percent_urban = census.df[,11]/census.df[,14]
-percent_rural = census.df[,13]/census.df[,14]
-
-percent_old = census.df[,68]/census.df[,15]
-
-#appears to be a downward trend
-percent_poor = census.df$Prs_Blw_Pov_Lev_ACS_06_10/census.df$Tot_Population_ACS_06_10
-
-percent_educated = census.df$College_ACS_06_10/census.df$Tot_Population_ACS_06_10
-
-#the percentage of spanish ballots mailed out, 167 is total number of ballots, 170 is spanish ballots
-spanishBallots = census.df[,170]
-spanishBallots[is.na(spanishBallots)] = 0
-percent_spanishBallots = spanishBallots/census.df[,167]
-
-#ballot difference
-ballot_diff = percent_spanish - percent_spanishBallots
-
-#171 refers to return rate category
-rate = census.df[,171]
-
-plot(percent_poor, rate,pch='.')
+## Data loading
+load('cleaned.dat')
 
 #plot
 require('ggplot2')
-test.df = read.csv('training_file_plus_location.csv')
-loc = test.df[,c(174,173,171)]
+category.index = grep("LATITUDE|LONGITUDE|Mail_Return_Rate", names(census.formatted.df)) 
+loc = census.formatted.df[,category.index]
 index = which(loc$Mail_Return_Rate_CEN_2010 < 60)
 loc[index,3] = 60
 qplot(loc$LONGITUDE,loc$LATITUDE,data=loc, colour=loc$Mail_Return_Rate_CEN_2010, size='.') + scale_color_gradient(low='blue', high='red')
