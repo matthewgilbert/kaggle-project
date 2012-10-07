a7201893bb06055bc65a345c300da7d522db5abe
Cameron Davidson-Pilon <cam.davidson.pilon@gmail.com>
2012-10-07 13:21:22 -0400

    added more quad features

--- python-code/quadratic_selection.py
+++ python-code/quadratic_selection.py
@@ -1,6 +1,12 @@
 #quadratic variable selection
 
 
+
+#
+# TODO: See if the coefficents of LR change much across the groups.
+
+#
+
 from itertools import combinations
 
 run data_explorying.py
@@ -14,15 +20,15 @@ n,d = vdata.shape
 
 qdata = np.zeros( (n, d*(d-1)/2 ) )
 
-for col_pos, i,j in enumerate( combinations( range(d), 2) ):
-    qdata[:, col_pos] = vdata[:,i]*v[:,j]
+for col_pos, i,j in enumerate( combinations( range(d*(d-1)/2), 2) ):
+    qdata[:, col_pos] = vdata[:,i]*vdata[:,j]
     ix.append( (i,j) )
     
 
     
-lr = sklm.Lasso( alpha = .001, normalize = True)
+lr = sklm.Lasso( alpha = .005, normalize = True)
 lr.fit( qdata, response)
 
-print "Percent of variables selected: %.2f"%( float(np.nonzero( lr.coef_ ).shape[0])/(d*(d-1)/2)
+print "Percent of variables selected: %.2f"%( float(np.nonzero( lr.coef_ )[0].shape[0])/(d*(d-1)/2) )
 print "indexes:"
 print np.nonzero( lr.coef_ )
